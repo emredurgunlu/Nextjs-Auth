84e0d69e7fbfc45690fe237790f4b1c42e2d2922
emre durgunlu <emredurgunlu@gmail.com>
2024-03-19 02:20:35 +0300

    update premium page

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,7 +1,28 @@
-const PremiumPage = () => {
+import { getSession } from "@/actions";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+const PremiumPage = async () => {
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
+    redirect("/");
+  }
+
+  if (!session.isPro) {
+    return (
+      <div className="notPremium">
+        <h1>Only premium users can see the content!</h1>
+        <Link href="/profile">
+          Go to the profile page to upgrade to premium
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className="home">
-      <h1>Welcome to the PremiumPage</h1>
+      <h1>Welcome to the Premium Page</h1>
+      <p>You can see this page because you are a premium user</p>
     </div>
   );
 };
