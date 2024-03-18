c3e38cd04f117bd2e3f83f0af16698608150ba2f
emre durgunlu <emredurgunlu@gmail.com>
2024-03-19 00:35:43 +0300

    check username for login
    type "emre" for username to login

--- src/actions.ts
+++ src/actions.ts
@@ -3,6 +3,12 @@
 import { sessionOptions, SessionData, defaultSession } from "@/lib";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+// Dummy Data
+let username = "emre";
+let isPro = true;
+let isBlocked = true;
 
 export const getSession = async () => {
     // <SessionData> interface olarak kalıba uymak için
@@ -16,5 +22,29 @@ export const getSession = async () => {
 
     return session;
 }
-export const login = async () => {}
+export const login = async (formData: FormData) => {
+
+    const session = await getSession();
+
+    const formUsername = formData.get("username") as string;
+    const formPassword = formData.get("password") as string;
+
+    // CHECK USER IN THE DB for the real project. But dummy data is used in this project
+    // const user = await db.getUser({username,password})
+
+    // Eğer formdaki username databasedeki username'e eşit değilse
+    if (formUsername !== username) {
+        return { error: "Wrong Credentials!" };
+    }
+
+    // Eğer formdaki username databasedeki username'e eşitse yani bir problem yoksa session bilgisini güncelliyoruz
+    session.userId = "1"; // gerçek projede bu id databaseden gelecek
+    session.username = formUsername;
+    session.isPro = isPro;
+    session.isLoggedIn = true;
+  
+    await session.save();
+    redirect("/");
+
+}
 export const logout = async () => {}
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,8 @@
+import { login } from "@/actions";
+
 const LoginForm = () => {
   return (
-    <form>
+    <form action={login}>
       <input type="text" name="username" required placeholder="username" />
       <input type="password" name="password" required placeholder="password" />
       <button>Login</button>
